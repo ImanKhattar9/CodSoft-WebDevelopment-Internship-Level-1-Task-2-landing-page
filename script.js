f3f3ed4d5785de1b05de50073db2760a5d3120b0
ImanKhattar9 <121335524+ImanKhattar9@users.noreply.github.com>
2024-02-08 21:47:58 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function imgSlider(anything){
+    document.querySelector('.drink').src = anything;
+}
+function changeBackgroundcolor(color){
+    const circle = document.querySelector('.circle');
+    circle.style.background = color;
+}
